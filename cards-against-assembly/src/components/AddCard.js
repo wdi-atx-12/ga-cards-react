64e7d7e12ced8f01da1345a1bcf7411cd4aa0ede
Matt <mattray0295@gmail.com>
2017-10-27 12:30:42 -0500

    added onchange to input on add card

--- cards-against-assembly/src/components/AddCard.js
+++ cards-against-assembly/src/components/AddCard.js
@@ -2,16 +2,34 @@ import React, { Component } from 'react';
 
 
 class AddCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: ''
+    };
+    
+    this._handleChange = this._handleChange.bind(this);
+  }
+
+
+  _handleChange() {
+    this.setState({
+      value: this.inputBox.value
+    })
+
+  }
+
   render(){
     return(
       <section className="container-fluid">
         <div className="row">
           <form name="add-card" id="add-card">
-            <input type="text" name="question" id="question" placeholder="What's your question?" />
+            <input onChange={this._handleChange} ref={(input) => this.inputBox = input} type="text" name="question" id="question" placeholder="What's your question?" />
           </form>
           <br />
           <div className="card">
-            <h4 className="card-title">A preview appears here as you type...</h4>
+            <h4 className="">{this.state.value}</h4>
             <h6>Cards Against Assembly</h6>
           </div>
         </div>
