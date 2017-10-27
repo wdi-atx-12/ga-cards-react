78862b2fa035d17cd94b3d14ccee47bd0897837e
Dazaev Vives <dazaev.vives.cervantes@gmail.com>
2017-10-27 12:13:25 -0500

    cards are hidden now

--- cards-against-assembly/src/components/Cards.js
+++ cards-against-assembly/src/components/Cards.js
@@ -1,39 +1,40 @@
 import React, { Component } from 'react';
 
 class Cards extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showCard: "card-title"
+    }
+    this._showCard = this._showCard.bind(this);
+  }
+  _showCard(e){
+    e.preventDefault();
+    if (this.state.showCard === "card-title") {
+      this.setState({
+        showCard: " "
+      })
+    } else {
+      this.setState({
+        showCard: "card-title"
+      })
+    }
+  }
+
   render(){
     return(
       <section id="cards" className="container-fluid">
 
-      <div className="row">
-        <div className="col-sm-6 col-md-4 col-lg-4">
-          <div className="card">
-            <h4 className="card-title">I couldnt complete my assignment because ________</h4>
-            <h6>Cards Against Assembly</h6>
+        <div className="row">
+          <div className="col-sm-6 col-md-4 col-lg-4">
+            <div onClick={this._showCard} className="card">
+              <h4 className={this.state.showCard}>I couldnt complete my assignment because ________</h4>
+              <h6>Cards Against Assembly</h6>
+            </div>
           </div>
         </div>
 
-        <div className="col-sm-6 col-md-4 col-lg-4">
-          <div className="card">
-            <h4 className="card-title">I get by with a little help from ________</h4>
-            <h6>Cards Against Assembly</h6>
-          </div>
-        </div>
-
-        <div className="col-sm-6 col-md-4 col-lg-4">
-          <div className="card">
-            <h4 className="card-title">The field trip was completely ruined by ________</h4>
-            <h6>Cards Against Assembly</h6>
-          </div>
-        </div>
-
-        <div className="col-sm-6 col-md-4 col-lg-4">
-          <div className="card">
-            <h4 className="card-title">Make food not _______________</h4>
-            <h6>Cards Against Assembly</h6>
-          </div>
-        </div>
-      </div>
       </section>
     )
   }
